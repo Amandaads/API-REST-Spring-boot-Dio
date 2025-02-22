e11153f041d38727579645235ab3c5baf55c8ae1
Amanda <amanda.profissional078@gmail.com>
2025-02-21 22:18:39 -0300

    Java RESTful API criada para Decola tech

---
+++ Dio-decola-tech/src/main/java/me/dio/Dio_decola_tech/DioDecolaTechApplication.java
@@ -0,0 +1,13 @@
+package me.dio.Dio_decola_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDecolaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDecolaTechApplication.class, args);
+	}
+
+}
